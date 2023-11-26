cd2490c34aac6384727357308cc8145600109706
MgrGracz <kamil.bandzwolek@gmail.com>
2023-11-26 21:46:14 +0100

    v001
    Startujemy

---
+++ bubble-chain/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lecimy z nowym projektem.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Grandpa1001/BubbleChain"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Grandpa1001
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
